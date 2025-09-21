2c67f1f87747783827b5d2abb87321fd927a7ed5
Гуща Иван <dimasanyavanya2005@gmail.com>
2025-09-21 23:17:13 +0300

    Update laba1.cpp

--- trunk/as06503/task_01/src/laba1.cpp
+++ trunk/as06503/task_01/src/laba1.cpp
@@ -1,19 +1,13 @@
-﻿//#include <iostream>
-//#include <cmath>
-//int main()
-//{
-//
-//}
 #include <iostream>
 #include <cmath>
 using namespace std;
 //константы
-int n = 10;// такты времени
+const int n = 10;// такты времени
 const double a = 0.99;
 const double b = 0.01;
 const double c = 0.5;
 const double d = 0.1;
-double Y0 = 18;
+const double Y0 = 18;
 
 //линейная
 double linear(double yt, double ut) {
@@ -30,16 +24,18 @@ int main() {
     cout << "Линейная модель"<<endl;
     cout << "y0 = " << Y0<<endl;
     double u[10] = {5,7,6,5,7,6,5,7,6,5};
-    double yt = Y0,yt_1=Y0;
+    double yt = Y0;
     for (int i=0;i<n;i++){
         yt = linear(yt, u[i]);
         cout << "y" << i+1 << " = " << yt<<endl;
     }
     cout << "\n";
+    double yt_1;
     yt_1=yt = Y0;
     cout << "Нелинейная модель" << endl;
     cout << "y0 = " << Y0 << endl;
     for (int i = 0; i < n-1; i++) {
+        yt_1=yt;
         yt = nonlinear(yt, yt_1, u[i+1], u[i]);
         cout << "y" << i+1 << " = " << yt << endl;
     }
