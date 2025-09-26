6318e7f8d043055d4849b17e6677a54645634036
MrDarknessT <wlksmb2006@mail.ru>
2025-09-26 14:24:19 +0300

    исправления LAB1.cpp после первой ревизии Copilot

--- trunk/as06509/task_01/src/LAB1.cpp
+++ trunk/as06509/task_01/src/LAB1.cpp
@@ -4,48 +4,51 @@
 
 using namespace std;
 
-int n = 10;  
+struct Constants{
+    int n = 10;  
 
-float a = 1.2;
-float b = 0.01;
-float c = 0.75;
-float d = 0.03;
+    const double a = 1.2;
+    const double b = 0.01;
+    const double c = 0.75;
+    const double d = 0.03;
+};
 
-vector<float> u(n + 1, 4.0);
-
-void linear() {
-    vector<float> y(n + 1, 0.0); 
+void linear(Constants &con, vector<double> &u) {
+    vector<double> y(con.n + 1, 0.0); 
     y[0] = 25;
 
-    for (int t = 0; t < n; ++t) {
-        y[t + 1] = a * y[t] + b * u[t];
+    for (int t = 0; t < con.n; ++t) {
+        y[t + 1] = con.a * y[t] + con.b * u[t];
     }
 
     cout << "Linear:\n";
-    for (int t = 0; t <= n; ++t) {
+    for (int t = 0; t <= con.n; ++t) {
         cout << "y" << t << ": " << y[t] << endl;
     }
 }
 
-void Nonlinear() {
-    vector<float> y(n + 1, 0.0); 
+void Nonlinear(Constants &con, vector<double> &u) {
+    vector<double> y(con.n + 1, 0.0); 
     y[0] = 25;
     y[1] = y[0];
 
-    for (int t = 1; t < n; ++t) {
-        y[t + 1] = a * y[t] - b * pow(y[t - 1], 2) + c * u[t] + d * sin(u[t - 1]);
+    for (int t = 1; t < con.n; ++t) {
+        y[t + 1] = con.a * y[t] - con.b * pow(y[t - 1], 2) + con.c * u[t] + con.d * sin(u[t - 1]);
     }
 
 
     cout << "\nNonlinear:\n";
-    for (int t = 0; t <= n; ++t) {
+    for (int t = 0; t <= con.n; ++t) {
         cout << "y" << t << ": " << y[t] << endl;
     }
 }
 
 int main() {
-    linear();
-    Nonlinear();
+    Constants con;
+    vector<double> u(con.n + 1, 4.0);
+
+    linear(con, u);
+    Nonlinear(con, u);
 
     return 0;
 }
