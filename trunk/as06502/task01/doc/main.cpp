d485c67af204325ec69c3d40cdf1ec3c5ef940e4
dadjil <grudas2005@gmail.com>
2025-09-29 10:21:03 +0300

    final with readme

---
+++ trunk/as06502/task01/doc/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+
+const int STEPS = 10;
+const double A = 0.2;
+const double B = 0.02;
+const double C = 0.03;
+const double D = 0.04;
+
+double calcLinear(double currentY, double inputU) {
+    return (A * currentY) + (B * inputU);
+}
+
+double calcNonlinear(double currentY, double prevY, double inputU, double prevU) {
+    return (A * currentY) - (B * prevY * prevY) + (C * inputU) + D * sin(prevU);
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double temperatureInitial = 20.0;
+
+    double inputs[STEPS] = {
+        15.0, 16.5, 18.0, 20.0, 22.5, 25.0, 24.0, 21.0, 18.0, 16.0
+    };
+
+    cout << "Линейная модель\nНачальное значение: " << temperatureInitial << "\n";
+
+    double prevTemperature = temperatureInitial;
+
+    for (int t = 0; t < STEPS; ++t) {
+        double newTemperature = calcLinear(prevTemperature, inputs[t]);
+        cout << "y[" << t + 1 << "] = " << newTemperature << endl;
+        prevTemperature = newTemperature;
+    }
+
+    cout << "\nНелинейная модель\nНачальное значение: " << temperatureInitial << "\n";
+
+    double currentY = temperatureInitial;
+    double previousY = temperatureInitial;
+
+    for (int t = 1; t < STEPS; ++t) {
+        double uCurr = inputs[t];
+        double uPrev = inputs[t-1];
+
+        double newY = calcNonlinear(currentY, previousY, uCurr, uPrev);
+        cout << "y[" << t << "] = " << newY << endl;
+
+        previousY = currentY;
+        currentY = newY;
+    }
+
+    system("pause");
+    return 0;
+}
