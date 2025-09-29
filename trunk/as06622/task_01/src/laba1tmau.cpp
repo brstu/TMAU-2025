7fb3badd29a54b44e33d6f6c7cc5aa4475f88d15
Mark Savinets <bambukbambukovic@gmail.com>
2025-09-29 16:01:15 +0300

    TMAU-lab1

---
+++ trunk/as06622/task_01/src/laba1tmau.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+// Параметры модели
+const int STEPS = 10;
+const double K1 = 0.99;
+const double K2 = 0.01;
+const double K3 = 0.5;
+const double K4 = 0.1;
+const double Y0 = 18.0;
+
+// Линейная динамика
+double linModel(double y, double u) {
+    return K1 * y + K2 * u;
+}
+
+// Нелинейная динамика
+double nonlinEq(double y, double y_prev, double u, double u_prev) {
+    return K1 * y + K3 * u - K2 * pow(y_prev, 2) + K4 * sin(u_prev);
+}
+
+void simulateLinear(const vector<double>& input) {
+    double y = Y0;
+    cout << "=== Линейная модель ===" << endl;
+    cout << "y0 = " << Y0 << endl;
+    for (int i = 0; i < STEPS; i++) {
+        y = linModel(y, input[i]);
+        cout << "Шаг " << i + 1 << ": y = " << y << endl;
+    }
+}
+
+void simulateNonlinear(const vector<double>& input) {
+    double y = Y0, y_prev = Y0;
+    cout << "\n=== Нелинейная модель ===" << endl;
+    cout << "y0 = " << Y0 << endl;
+    for (int i = 0; i < STEPS - 1; i++) {
+        y = nonlinEq(y, y_prev, input[i + 1], input[i]);
+        cout << "Шаг " << i + 1 << ": y = " << y << endl;
+        y_prev = y;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    vector<double> input = { 5,7,6,5,7,6,5,7,6,5 };
+    simulateLinear(input);
+    simulateNonlinear(input);
+    return 0;
+}
