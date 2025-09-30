7bc66cac755ca15917bdd040089aaa8353384591
tnir-code <cevan2006@mail.ru>
2025-09-30 22:14:57 +0300

    сделал задание 1

--- as06609/task_01/src/tmay11.cpp
+++ as06609/task_01/src/tmay11.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <cmath>
-using namespace std;
+
 const int STEPS = 10;
 const double K1 = 0.98;
 const double K2 = 0.03;
@@ -14,33 +14,33 @@ double linModel(double y, double u) {
 }
 
 double nonlinEq(double y, double y_prev, double u, double u_prev) {
-    return K1 * y + K3 * u - K2 * pow(y_prev, 2) + K4 * sin(u_prev);
+    return K1 * y + K3 * u - K2 * std::pow(y_prev, 2) + K4 * std::sin(u_prev);
 }
 
-void simulateLinear(const vector<double>& input) {
+void simulateLinear(const std::vector<double>& input) {
     double y = Y0;
-    cout << "=== Линейная модель ===" << endl;
-    cout << "y0 = " << Y0 << endl;
+    std::cout << "=== Линейная модель ===" << std::endl;
+    std::cout << "y0 = " << Y0 << std::endl;
     for (int i = 0; i < STEPS; i++) {
         y = linModel(y, input[i]);
-        cout << "Шаг " << i + 1 << ": y = " << y << endl;
+        std::cout << "Шаг " << i + 1 << ": y = " << y << std::endl;
     }
 }
 
-void simulateNonlinear(const vector<double>& input) {
+void simulateNonlinear(const std::vector<double>& input) {
     double y = Y0, y_prev = Y0;
-    cout << "\n=== Нелинейная модель ===" << endl;
-    cout << "y0 = " << Y0 << endl;
+    std::cout << "\n=== Нелинейная модель ===" << std::endl;
+    std::cout << "y0 = " << Y0 << std::endl;
     for (int i = 1; i < STEPS; i++) {
         y = nonlinEq(y, y_prev, input[i], input[i - 1]);
-        cout << "Шаг " << i << ": y = " << y << endl;
+        std::cout << "Шаг " << i << ": y = " << y << std::endl;
         y_prev = y;
     }
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    vector<double> input = { 5,7,6,5,7,6,5,7,6,5 };
+    std::setlocale(LC_ALL, "Russian");
+    std::vector<double> input = { 5,7,6,5,7,6,5,7,6,5 };
     simulateLinear(input);
     simulateNonlinear(input);
     return 0;
