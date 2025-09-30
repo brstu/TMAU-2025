0edc7290c3f5e9c7919770888647ebd6e1805fdd
afterd1ce <lysyuk.rostislav@mail.ru>
2025-09-30 11:29:55 +0300

    Обновил код

--- as06611/task_01/src/LAB1.cpp
+++ as06611/task_01/src/LAB1.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 #include <cmath>
 
-using namespace std;
-
 class TemperatureModel {
 private:
     double a, b, c, d;
@@ -22,7 +20,7 @@ public:
     }
 
     double nonlinearModel(double u, double u_prev) {
-        double y_next = a * y_curr - b * pow(y_prev, 2) + c * u + d * sin(u_prev);
+        double y_next = a * y_curr - b * std::pow(y_prev, 2) + c * u + d * std::sin(u_prev);
         y_prev = y_curr;
         y_curr = y_next;
         return y_next;
@@ -40,23 +38,23 @@ int main() {
     double c = 0.3;
     double d = 0.1;
 
-    vector<double> u = { 1.0, 1.2, 0.8, 1.5, 1.0, 0.7, 1.3, 0.9, 1.1, 0.6 };
+    std::vector<double> u = { 1.0, 1.2, 0.8, 1.5, 1.0, 0.7, 1.3, 0.9, 1.1, 0.6 };
 
     TemperatureModel model(a, b, c, d);
 
-    cout << "Linear model:" << endl;
+    std::cout << "Linear model:" << std::endl;
     model.reset();
     for (size_t i = 0; i < u.size(); ++i) {
         double y = model.linearModel(u[i]);
-        cout << "t=" << (i + 1) << ", u=" << u[i] << ", y=" << y << endl;
+        std::cout << "t=" << (i + 1) << ", u=" << u[i] << ", y=" << y << std::endl;
     }
 
-    cout << "\nNonlinear model:" << endl;
+    std::cout << "\nNonlinear model:" << std::endl;
     model.reset();
     for (size_t i = 0; i < u.size(); ++i) {
         double u_prev = (i == 0) ? 0 : u[i - 1];
         double y = model.nonlinearModel(u[i], u_prev);
-        cout << "t=" << (i + 1) << ", u=" << u[i] << ", y=" << y << endl;
+        std::cout << "t=" << (i + 1) << ", u=" << u[i] << ", y=" << y << std::endl;
     }
 
     return 0;
