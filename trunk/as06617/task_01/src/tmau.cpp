2f5abb512f315f3caff94c050a27a810d76876c1
arseniiOsovets <arsenasavets@gmail.com>
2025-10-02 10:23:38 +0300

    chore: some fix

--- trunk/as06617/task_01/src/tmau.cpp
+++ trunk/as06617/task_01/src/tmau.cpp
@@ -4,44 +4,44 @@
 
 using namespace std;
 
+struct Coeffs {
+    double a;
+    double b;
+    double c;
+    double d;
+};
 
 double linear_model(double y_t, double u_t, double a, double b) {
     return a * y_t + b * u_t;
 }
 
-double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev,
-    double a, double b, double c, double d) {
-    return a * y_t - b * y_prev * y_prev + c * u_t + d * sin(u_prev);
+double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev, Coeffs coeffs) {
+    return coeffs.a * y_t - coeffs.b * y_prev * y_prev + coeffs.c * u_t + coeffs.d * sin(u_prev);
 }
 
 int main() {
-    double a = 0.8;
-    double b = 0.2;
-    double c = 0.1;
-    double d = 0.05;
+    Coeffs coeffs = {0.8, 0.2, 0.1, 0.05};
 
     int N = 20; 
 
     vector<double> y(N + 1, 0.0); 
     vector<double> u(N + 1, 0.0); 
 
-    
     for (int t = 0; t <= N; t++) {
-        if (t < 5) u[t] = 0.0;
-        else u[t] = 1.0; 
+        u[t] = (t < 5) ? 0.0 : 1.0;
     }
 
-    cout << "linear model" << endl;
+    cout << "Linear model" << endl;
     for (int t = 0; t < N; t++) {
-        y[t + 1] = linear_model(y[t], u[t], a, b);
+        y[t + 1] = linear_model(y[t], u[t], coeffs.a, coeffs.b);
         cout << "t=" << t + 1 << "  y=" << y[t + 1] << endl;
     }
 
     fill(y.begin(), y.end(), 0.0);
 
-    cout << "\n non linear model" << endl;
+    cout << "\nNonlinear model" << endl;
     for (int t = 1; t < N; t++) {
-        y[t + 1] = nonlinear_model(y[t], y[t - 1], u[t], u[t - 1], a, b, c, d);
+        y[t + 1] = nonlinear_model(y[t], y[t - 1], u[t], u[t - 1], coeffs);
         cout << "t=" << t + 1 << "  y=" << y[t + 1] << endl;
     }
 
