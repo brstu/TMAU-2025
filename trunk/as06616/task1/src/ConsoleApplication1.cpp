41fba94fa07f570987b604bea7aff2ef256179ce
nklvmmr <z3594934z@gmail.com>
2025-10-02 10:09:02 +0300

    Задание 1 ТиМАУ Николова испр

--- trunk/as06616/task1/src/ConsoleApplication1.cpp
+++ trunk/as06616/task1/src/ConsoleApplication1.cpp
@@ -7,15 +7,23 @@ const double b = 0.3;
 const double c = 0.8;
 const double d = 0.5;
 
+
 double linearmodel(double inputtemperature, double inputwarm) {
     return a * inputtemperature + b * inputwarm;
 }
+
 
 double nonlinearmodel(double inputtemperature, double prevtemperature, double inputwarm, double prevwarm) {
     return a * inputtemperature - b * pow(prevtemperature, 2) + c * inputwarm + d * sin(prevwarm);
 }
 
+
+double updateWarm(double baseWarm, int t) {
+    return baseWarm + 0.1 * sin(t); 
+}
+
 int main() {
+
     int time;
     double inputtemperature;
     double inputwarm;
@@ -23,24 +31,25 @@ int main() {
     cout << "Enter simulation time steps, initial temperature y0, and initial warm input u0: ";
     cin >> time >> inputtemperature >> inputwarm;
 
-    double prevtemperature = inputtemperature; 
-    double prevwarm = inputwarm;              
+    double prevtemperature = inputtemperature;
+    double prevwarm = inputwarm;
 
     cout << "\nLinear Model Simulation:\n";
     double y_linear = inputtemperature;
     for (int t = 1; t <= time; t++) {
         y_linear = linearmodel(y_linear, inputwarm);
-        cout << "Step " << t << ": y = " << y_linear << endl;
+        cout << "Шаг " << t << ": y = " << y_linear << endl;
     }
 
     cout << "\nNonlinear Model Simulation:\n";
     double y_nl = inputtemperature;
     for (int t = 1; t <= time; t++) {
-        double next = nonlinearmodel(y_nl, prevtemperature, inputwarm, prevwarm);
+        double currentWarm = updateWarm(inputwarm, t); 
+        double next = nonlinearmodel(y_nl, prevtemperature, currentWarm, prevwarm);
         cout << "Step " << t << ": y = " << next << endl;
         prevtemperature = y_nl;
         y_nl = next;
-        prevwarm = inputwarm;
+        prevwarm = currentWarm;
     }
 
     return 0;
