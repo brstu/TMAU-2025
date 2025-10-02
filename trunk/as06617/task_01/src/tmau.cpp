bc1687a0261165a958edf8b01d486fdb280e44f6
arseniiOsovets <arsenasavets@gmail.com>
2025-10-02 10:27:02 +0300

    chore: some fix

--- trunk/as06617/task_01/src/tmau.cpp
+++ trunk/as06617/task_01/src/tmau.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <cmath>
-
+#include <iomanip>
 using namespace std;
 
 struct Coeffs {
@@ -15,15 +15,13 @@ double linear_model(double y_t, double u_t, double a, double b) {
     return a * y_t + b * u_t;
 }
 
-double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev, Coeffs coeffs) {
+double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev, const Coeffs& coeffs) {
     return coeffs.a * y_t - coeffs.b * y_prev * y_prev + coeffs.c * u_t + coeffs.d * sin(u_prev);
 }
 
 int main() {
     Coeffs coeffs = {0.8, 0.2, 0.1, 0.05};
-
     int N = 20; 
-
     vector<double> y(N + 1, 0.0); 
     vector<double> u(N + 1, 0.0); 
 
@@ -34,7 +32,7 @@ int main() {
     cout << "Linear model" << endl;
     for (int t = 0; t < N; t++) {
         y[t + 1] = linear_model(y[t], u[t], coeffs.a, coeffs.b);
-        cout << "t=" << t + 1 << "  y=" << y[t + 1] << endl;
+        cout << "t=" << t + 1 << "  y=" << fixed << setprecision(4) << y[t + 1] << endl;
     }
 
     fill(y.begin(), y.end(), 0.0);
@@ -42,7 +40,7 @@ int main() {
     cout << "\nNonlinear model" << endl;
     for (int t = 1; t < N; t++) {
         y[t + 1] = nonlinear_model(y[t], y[t - 1], u[t], u[t - 1], coeffs);
-        cout << "t=" << t + 1 << "  y=" << y[t + 1] << endl;
+        cout << "t=" << t + 1 << "  y=" << fixed << setprecision(4) << y[t + 1] << endl;
     }
 
     return 0;
