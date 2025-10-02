9e8d7e5a4ab76d2746bea2a1d55dc2a10291ab8a
MarkOsovets <markosovets@gmail.com>
2025-10-02 16:23:45 +0300

    laba1..

--- trunk/as6513/task_01/src/main.cpp
+++ trunk/as6513/task_01/src/main.cpp
@@ -9,11 +9,13 @@ double linear_model(double y_t, double u_t, double a, double b) {
     return a * y_t + b * u_t;
 }
 
+
 // Нелинейная модель
 double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev,
                        double a, double b, double c, double d) {
     return a * y_t - b * y_prev * y_prev + c * u_t + d * sin(u_prev);
 }
+
 
 int main() {
     // Константы модели
