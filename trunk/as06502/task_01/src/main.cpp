7bd3af4b338457cf7df5dbd0a2aa4e9e78f0bcb5
dadjil <grudas2005@gmail.com>
2025-10-06 10:21:41 +0300

    [task_01] Refactored

--- trunk/as06502/task_01/src/main.cpp
+++ trunk/as06502/task_01/src/main.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
-struct Constants{
-const double A = 0.2;
-const double B = 0.02;
-const double C = 0.03;
-const double D = 0.04;
-};
+namespace Constants {
+    const double A = 0.2;
+    const double B = 0.02;
+    const double C = 0.03;
+    const double D = 0.04;
+}
 
 double calcLinear(double currentY, double inputU) {
-    Constants c = Constants();
-    return (c.A * currentY) + (c.B * inputU);
+    return (Constants::A * currentY) + (Constants::B * inputU);
 }
 
 double calcNonlinear(double currentY, double prevY, double inputU, double prevU) {
-    Constants c = Constants();
-    return (c.A * currentY) - (c.B * prevY * prevY) + (c.C * inputU) + c.D * sin(prevU);
+    return (Constants::A * currentY) - (Constants::B * prevY * prevY) + (Constants::C * inputU) + Constants::D * sin(prevU);
 }
 
 int main() {
     setlocale(LC_ALL, "RU");
 
-    const int STEPS = 10;
     double temperatureInitial = 20.0;
-    const Constants c = Constants();
 
-    double inputs[STEPS] = {
+    vector<double> inputs = {
         15.0, 16.5, 18.0, 20.0, 22.5, 25.0, 24.0, 21.0, 18.0, 16.0
     };
 
@@ -34,7 +31,7 @@ int main() {
 
     double prevTemperature = temperatureInitial;
 
-    for (int t = 0; t < STEPS; ++t) {
+    for (int t = 0; t < inputs.size(); ++t) {
         double newTemperature = calcLinear(prevTemperature, inputs[t]);
         cout << "y[" << t + 1 << "] = " << newTemperature << endl;
         prevTemperature = newTemperature;
@@ -45,7 +42,7 @@ int main() {
     double currentY = temperatureInitial;
     double previousY = temperatureInitial;
 
-    for (int t = 1; t < STEPS; ++t) {
+    for (int t = 1; t < inputs.size(); ++t) {
         double uCurr = inputs[t];
         double uPrev = inputs[t-1];
 
@@ -59,4 +56,3 @@ int main() {
     system("pause");
     return 0;
 }
-//pls claim it
