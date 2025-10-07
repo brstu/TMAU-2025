4cbc603673deba27d89a825019a2eecd71f9a223
Artem Aroma <artemaroma44@gmail.com>
2025-10-08 02:34:20 +0300

    Добавил 1 лабу

---
+++ trunk/as06625/task_01/scr/tmau.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <cmath>
+using std::cout;
+using std::endl;
+
+// Константы
+const int TIME_STEPS = 10; // количество временных шагов
+const double COEFF_A = 0.99;
+const double COEFF_B = 0.01;
+const double COEFF_C = 0.5;
+const double COEFF_D = 0.1;
+const double INITIAL_Y = 18;
+
+// Линейная модель
+double compute_linear(double current_y, double input_u) {
+    return COEFF_A * current_y + COEFF_B * input_u;
+}
+
+// Нелинейная модель
+double compute_nonlinear(double current_y, double previous_y, double input_u, double previous_u) {
+    return COEFF_A * current_y - COEFF_B * pow(previous_y, 2) + COEFF_C * input_u + COEFF_D * sin(previous_u);
+}
+
+int main() {
+    system("chcp 65001");
+    cout << "Линейная модель" << endl;
+    cout << "y0 = " << INITIAL_Y << endl;
+
+    double inputs[TIME_STEPS] = { 5, 7, 6, 5, 7, 6, 5, 7, 6, 5 };
+    double current_y = INITIAL_Y;
+
+    for (int i = 0; i < TIME_STEPS; i++) {
+        current_y = compute_linear(current_y, inputs[i]);
+        cout << "y" << i + 1 << " = " << current_y << endl;
+    }
+
+    cout << "\n";
+
+    double previous_y = INITIAL_Y;
+    current_y = INITIAL_Y;
+
+    cout << "Нелинейная модель" << endl;
+    cout << "y0 = " << INITIAL_Y << endl;
+
+    for (int i = 0; i < TIME_STEPS; i++) {
+        double current_u = inputs[i];
+        double previous_u = (i == 0) ? inputs[0] : inputs[i - 1];
+        double new_y = compute_nonlinear(current_y, previous_y, current_u, previous_u);
+
+        cout << "y" << i + 1 << " = " << new_y << endl;
+
+        previous_y = current_y;
+        current_y = new_y;
+    }
+
+    return 0;
+}
