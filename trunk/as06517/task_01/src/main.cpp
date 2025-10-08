f764e9fcf2e875af779caec8bb90e4e68efb50a7
Егоренков Никита <egorenkovnikita0@gmail.com>
2025-10-08 13:36:08 +0300

    [task_01] fourth try

--- trunk/as06517/task_01/src/main.cpp
+++ trunk/as06517/task_01/src/main.cpp
@@ -3,17 +3,23 @@
 #include <iomanip>
 #include <vector>
 
-double linear_model(double y_prev, double u_tau, double a, double b) {
-    return a * y_prev + b * u_tau;
+struct Constants
+{
+    const double a = 0.8, b = 0.2, c = 0.25, d = 0.1;
+};
+
+
+double linear_model(double y_prev, double u_tau, Constants c) {
+    return c.a * y_prev + c.b * u_tau;
 }
 
 double nonlinear_model(double y_prev, double y_prev_2, double u_prev, 
-                     double u_tau, double a, double b, double c, double d) {
-    return a * y_prev - b * pow(y_prev_2, 2) + c * u_tau + d * sin(u_prev);
+                     double u_tau, Constants con) {
+    return con.a * y_prev - con.b * pow(y_prev_2, 2) + con.c * u_tau + con.d * sin(u_prev);
 }
 
 int main() {
-    const double a = 0.8, b = 0.2, c = 0.25, d = 0.1;
+    Constants con;
     double u_prev = 0, u_tau;
     int n;
     double y_prev = 20, y_prev_2 = 20;
@@ -25,7 +31,7 @@ int main() {
     std::cout << "=== Temperature Control Object Simulation ===" << std::endl;
     std::cout << "Linear model: y_{τ+1} = a*y_τ + b*u_τ" << std::endl;
     std::cout << "Nonlinear model: y_{τ+1} = a*y_τ - b*y_{τ-1}² + c*u_τ + d*sin(u_{τ-1})" << std::endl;
-    std::cout << "Parameters: a=" << a << ", b=" << b << ", c=" << c << ", d=" << d << std::endl;
+    std::cout << "Parameters: a=" << con.a << ", b=" << con.b << ", c=" << con.c << ", d=" << con.d << std::endl;
     std::cout << "Initial temperature: " << y_prev << "°C" << std::endl << std::endl;
 
     std::cout << "Enter number of steps: ";
@@ -48,9 +54,9 @@ int main() {
     for (size_t i = 0; i < n; i++) {
         u_tau = u_vec[i];
 
-        double linear_temp = linear_model(y_prev_linear, u_tau, a, b);
+        double linear_temp = linear_model(y_prev_linear, u_tau, con);
         double nonlinear_temp = nonlinear_model(y_prev_nonlinear, y_prev_2,
-                                            u_prev, u_tau, a, b, c, d);
+                                            u_prev, u_tau, con);
         
         linear_result.push_back(linear_temp);
         nonlinear_result.push_back(nonlinear_temp);
