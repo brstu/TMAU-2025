c3630ef6b17f8238caeb632fa765805959904e79
Янчук Артём <vladzhyk72@gmail.com>
2025-10-08 11:04:25 +0300

    sonarcloud2

--- trunk/as06624/task_01/src/tmau.cpp
+++ trunk/as06624/task_01/src/tmau.cpp
@@ -4,30 +4,31 @@
 
 using namespace std;
 
+// Структура для хранения коэффициентов модели
+struct ModelParams {
+    double a;
+    double b;
+    double c;
+    double d;
+};
 
-// Implements the linear model equation: y[τ+1] = a * y[τ] + b * u[τ]
-double linear_model(double y_t, double u_t, double a, double b) {
-    return a * y_t + b * u_t;
+// Линейная модель: y[τ+1] = a * y[τ] + b * u[τ]
+double linear_model(double y_t, double u_t, const ModelParams& params) {
+    return params.a * y_t + params.b * u_t;
 }
 
-// Nonlinear model:
-//   y[τ+1] = a*y[τ] - b*y[τ-1]^2 + c*u[τ] + d*sin(u[τ-1])
-// where:
-//   y[τ]     = current output
-//   y[τ-1]   = previous output
-//   u[τ]     = current input
-//   u[τ-1]   = previous input
-//   a, b, c, d = model parameters
+// Нелинейная модель:
+// y[τ+1] = a*y[τ] - b*y[τ-1]^2 + c*u[τ] + d*sin(u[τ-1])
 double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev,
-    double a, double b, double c, double d) {
-    return a * y_t - b * y_prev * y_prev + c * u_t + d * sin(u_prev);
+    const ModelParams& params) {
+    return params.a * y_t
+        - params.b * y_prev * y_prev
+        + params.c * u_t
+        + params.d * sin(u_prev);
 }
 
 int main() {
-    double a = 0.8;
-    double b = 0.2;
-    double c = 0.1;
-    double d = 0.05;
+    ModelParams params{ 0.8, 0.2, 0.1, 0.05 };
 
     int N = 20;
 
@@ -41,17 +42,18 @@ int main() {
 
     cout << "linear model" << endl;
     for (int t = 0; t < N; t++) {
-        y[t + 1] = linear_model(y[t], u[t], a, b);
+        y[t + 1] = linear_model(y[t], u[t], params);
         cout << "t=" << t + 1 << "  y=" << y[t + 1] << endl;
     }
 
     fill(y.begin(), y.end(), 0.0);
 
-    cout << "\n nonlinear model" << endl;
+    cout << "\nnonlinear model" << endl;
     for (int t = 1; t < N; t++) {
-        y[t + 1] = nonlinear_model(y[t], y[t - 1], u[t], u[t - 1], a, b, c, d);
+        y[t + 1] = nonlinear_model(y[t], y[t - 1], u[t], u[t - 1], params);
         cout << "t=" << t + 1 << "  y=" << y[t + 1] << endl;
     }
 
     return 0;
 }
+
