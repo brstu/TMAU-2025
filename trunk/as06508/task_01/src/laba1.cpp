0a43d86772134700834de2b8d3e9f88579519214
Кисель Максим <maximk220066@icloud.com>
2025-10-17 19:36:33 +0300

    Update laba1.cpp

--- trunk/as06508/task_01/src/laba1.cpp
+++ trunk/as06508/task_01/src/laba1.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 struct ModelParams {
+    const double INPUT_OFFSET = 10.0;
+    const double INPUT_AMPLITUDE = 2.0;
+    const double INPUT_FREQUENCY = 0.5;
+    const double INPUT_E = 0.95;
     const double a = 0.85;
     const double b = 0.002;
     const double c = 0.6;
@@ -24,7 +28,7 @@ void simulateAndPrint(const ModelParams& params, const vector<double>& u, bool i
         cout << "Linear Model Results:\n";
     }
     else {
-        y[1] = y[0] * 0.95;
+        y[1] = y[0] * params.INPUT_E;
         for (int t = 1; t < params.time_steps; ++t) {
             y[t + 1] = params.a * y[t] - params.b * pow(y[t - 1], 2) + params.c * u[t] + params.d * sin(u[t - 1]);
         }
@@ -33,7 +37,7 @@ void simulateAndPrint(const ModelParams& params, const vector<double>& u, bool i
 
     cout << "Time\tTemperature\n----------------\n";
     for (int t = 0; t <= params.time_steps; ++t) {
-        cout << t << "\t" << y[t] << endl; 
+        cout << t << "\t" << y[t] << endl;
     }
     cout << endl;
 }
@@ -43,7 +47,7 @@ int main() {
     vector<double> u(params.time_steps);
 
     for (int t = 0; t < params.time_steps; ++t) {
-        u[t] = 10.0 + 2.0 * sin(t * 0.5);
+        u[t] = params.INPUT_OFFSET + params.INPUT_AMPLITUDE * sin(t * params.INPUT_FREQUENCY);
     }
 
     simulateAndPrint(params, u, true);
