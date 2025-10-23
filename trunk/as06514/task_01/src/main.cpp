0ce7d3e2e0a5e2dc6514e8cd35544cad16f13bde
Вакурова Елизавета <batveta@mail.ru>
2025-10-23 13:34:18 +0300

    Update main.cpp

--- trunk/as06514/task_01/src/main.cpp
+++ trunk/as06514/task_01/src/main.cpp
@@ -6,20 +6,26 @@ using namespace std;
 
 class TemperatureModel {
 private:
-    double a, b, c, d;          
-    double startTemp;           
-    vector<double> heatInput;   
+    double a;
+    double b;
+    double c;
+    double d;
+    double startTemp;
+    vector<double> heatInput;
 
 public:
-    TemperatureModel(double a_val, double b_val, double c_val, double d_val, 
-                    double temp, vector<double> inputs) 
-        : a(a_val), b(b_val), c(c_val), d(d_val), 
-          startTemp(temp), heatInput(inputs) {}
+    TemperatureModel(double a_val, double b_val, double c_val, double d_val,
+                    double temp, const vector<double>& inputs)  // Передаём по константной ссылке
+        : a(a_val),
+          b(b_val),
+          c(c_val),
+          d(d_val),
+          startTemp(temp),
+          heatInput(inputs) {}
 
-    // Method for running a linear model
     void runLinear() {
         cout << "Linear model results:" << endl;
-        double current = startTemp;  
+        double current = startTemp;
         
         for (int i = 0; i < heatInput.size(); i++) {
             current = a * current + b * heatInput[i];
@@ -27,20 +33,19 @@ public:
         }
     }
 
-    // Method for running a nonlinear model
     void runNonlinear() {
         cout << "\nResults of the nonlinear model:" << endl;
-        double prev = startTemp;     
-        double current = startTemp;  
+        double prev = startTemp;
+        double current = startTemp;
         
         for (int i = 0; i < heatInput.size(); i++) {
             double prevHeat = (i > 0) ? heatInput[i - 1] : 0;
 
-            double next = a * current + b * prev + c * heatInput[i] + d * sin(prevHeat);
+            double next = a * current - b * prev + c * heatInput[i] + d * sin(prevHeat);
             cout << "Y" << i + 1 << ": " << next << endl;
 
-            prev = current;     
-            current = next;     
+            prev = current;
+            current = next;
         }
     }
 };
@@ -52,7 +57,6 @@ int main() {
     TemperatureModel model(0.7, 0.3, 0.05, 0.01, 20.0, inputs);
   
     model.runLinear();
-
     model.runNonlinear();
     
     return 0;
