09810a91759c357eed714000d23deeacc6ffdc09
Антон Горобец <xivorblr@gmail.com>
2025-10-29 23:25:39 +0300

    task2

---
+++ trunk/as06605/task_02/src/function.cpp
@@ -0,0 +1,28 @@
+#include "function.h"
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector <double> linear(double u)
+{
+	vector <double> temps(iterations);
+	temps[0] = InpTemp;
+	for (int i = 0; i < iterations - 1; ++i)
+	{
+		temps[i + 1] = a * temps[i] + b * u;
+	}
+	return temps;
+}
+
+vector <double> nonlinear(double u)
+{
+	vector <double> temps(iterations);
+	temps[0] = InpTemp;
+	// Initialize temps[1] separately to avoid out-of-bounds access for temps[i-1]
+	temps[1] = a * temps[0] - b * temps[0] * temps[0] + c * u + d * sin(u);
+	for (int i = 1; i < iterations - 1; ++i)
+	{
+		temps[i + 1] = a * temps[i] - b * temps[i - 1] * temps[i - 1] + c * u + d * sin(u);
+	}
+	return temps;
+}
