779b88180c7f3eac836744c92ba4ec6a3347f3b6
Nastya <204497599+Anastasia05-j@users.noreply.github.com>
2025-10-31 23:24:33 +0300

    ОЧЕНЬ красивое

--- trunk/as06619/task_01/src/lab1.cpp
+++ trunk/as06619/task_01/src/lab1.cpp
@@ -4,13 +4,23 @@
 
 using namespace std;
 
+struct NonlinearParams {
+    double y_t;
+    double y_prev;
+    double u_t;
+    double u_prev;
+    double a;
+    double b;
+    double c;
+    double d;
+};
+
 double compute_linear_model(double y_t, double u_t, double a, double b) {
     return a * y_t + b * u_t;
 }
 
-double compute_nonlinear_model(double y_t, double y_prev, double u_t, double u_prev,
-    double a, double b, double c, double d) {
-    return a * y_t - b * y_prev * y_prev + c * u_t + d * sin(u_prev);
+double compute_nonlinear_model(const NonlinearParams& p) {
+    return p.a * p.y_t - p.b * p.y_prev * p.y_prev + p.c * p.u_t + p.d * sin(p.u_prev);
 }
 
 int main() {
@@ -38,7 +48,17 @@ int main() {
 
     cout << "\nNonlinear model:" << endl;
     for (int t = 1; t < N; t++) {
-        y[t + 1] = compute_nonlinear_model(y[t], y[t - 1], u[t], u[t - 1], a, b, c, d);
+        NonlinearParams p;
+        p.y_t = y[t];
+        p.y_prev = y[t - 1];
+        p.u_t = u[t];
+        p.u_prev = u[t - 1];
+        p.a = a;
+        p.b = b;
+        p.c = c;
+        p.d = d;
+
+        y[t + 1] = compute_nonlinear_model(p);
         cout << "t = " << t + 1 << "   y = " << y[t + 1] << endl;
     }
 
