dc9195b1a22451075d1f3367a130ddc0a9d8baef
Kirill <kirillarmak7@gmail.com>
2025-10-31 23:56:25 +0300

    Edited src/

--- trunk/as06523/task_02/src/model.cpp
+++ trunk/as06523/task_02/src/model.cpp
@@ -5,7 +5,6 @@ double linear_model(double y, double u, double a, double b) {
     return a * y + b * u;
 }
 
-double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev,
-                       double a, double b, double c, double d) {
-    return a * y_t - b * y_prev * y_prev + c * u_t + d * sin(u_prev);
+double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev, const Coeffs& coeffs) {
+    return coeffs.a * y_t - coeffs.b * y_prev * y_prev + coeffs.c * u_t + coeffs.d * sin(u_prev);
 }
--- trunk/as06523/task_02/src/model.h
+++ trunk/as06523/task_02/src/model.h
@@ -1,7 +1,14 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+struct Coeffs {
+    double a;
+    double b;
+    double c;
+    double d;
+};
+
 double linear_model(double y, double u, double a, double b);
-double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev, double a, double b, double c, double d);
+double nonlinear_model(double y_t, double y_prev, double u_t, double u_prev, const Coeffs& coeffs);
 
 #endif
