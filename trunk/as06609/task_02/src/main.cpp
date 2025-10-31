8aa48b30ff7b719537e2634f2dfa2200bb461307
Nikita Kugan <kugannikita2006@gmail.com>
2025-10-31 17:01:34 +0300

    исправление

--- trunk/as06609/task_02/src/main.cpp
+++ trunk/as06609/task_02/src/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <array>        
 #include "func.h"
 using std::cout;
 using std::endl;
 
 int main() {
-    double u[N] = { 5, 7, 6, 5, 7, 6, 5, 7, 6, 5 };
+    std::array<double, N> u = { 5, 7, 6, 5, 7, 6, 5, 7, 6, 5 };  
     double y = Y_START;
     double y_prev = Y_START;
 
     cout << "Линейная модель" << endl;
     cout << "y0 = " << Y_START << endl;
 
-    for (int i = 0; i < N; i++) {
+    for (size_t i = 0; i < u.size(); i++) {   
         y = linearModel(y, u[i]);
         cout << "y" << i + 1 << " = " << y << endl;
     }
@@ -20,7 +21,7 @@ int main() {
     cout << "y0 = " << Y_START << endl;
 
     y = y_prev = Y_START;
-    for (int i = 0; i < N - 1; i++) {
+    for (size_t i = 0; i < u.size() - 1; i++) {
         y = nonlinearModel(y, y_prev, u[i + 1], u[i]);
         cout << "y" << i + 1 << " = " << y << endl;
         y_prev = y;
