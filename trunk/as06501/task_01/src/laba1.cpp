9dd8a17a389dbbebca77efd8e2ecfd314fd049a0
Вавдийчик Никита <vavdiychik01@gmail.com>
2025-11-13 18:36:10 +0300

    Update laba1.cpp

--- trunk/as06501/task_01/src/laba1.cpp
+++ trunk/as06501/task_01/src/laba1.cpp
@@ -2,32 +2,36 @@
 #include <cmath>
 #include <iomanip>
 #include <array>
+#include <vector>
 using namespace std;
 
-const int N = 10;
+const int N = 8;  
 const double a = 0.85;      
 const double b = 0.05;       
 const double c = 0.35;       
 const double d = 0.12;      
-const double start_value = 18;    
+const double start_value = 10;  
+
 
 double linear(double y, double u) {
     return a * y + b * u;
 }
 
 double nonlinear(double yt, double yt1, double ut, double ut1) {
-    return a * yt - b * pow(yt1, 1.5) + c * ut + d * sin(ut1); 
+    return a * yt - b * pow(yt1, 2) + c * ut + d * sin(ut1);  
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    std::array<double, N> u = { 3.0, 5.0, 4.0, 6.0, 5.0, 7.0, 4.0, 6.0, 5.0, 4.0 };
+    std::array<double, N> u = { 2.0, 4.0, 3.0, 5.0, 4.0, 6.0, 3.0, 5.0 };
 
-    cout << fixed << setprecision(3);
+    cout << fixed << setprecision(4);  
     
+    cout << "=== МОДЕЛЬ 1 (Задача 1) ===" << endl;
     cout << "Линейная модель:" << endl;
     cout << "y0 = " << start_value << endl;
+    
     double y = start_value;
     for (int i = 0; i < N; i++) {
         y = linear(y, u[i]);
@@ -36,24 +40,17 @@ int main() {
 
     cout << "\nНелинейная модель:" << endl;
     cout << "y0 = " << start_value << endl;
-    double y_prev = start_value;
-    double y_curr = start_value;
     
-   
-    double y_next = nonlinear(y_curr, y_prev, u[0], u[0]);
-    cout << "y1 = " << y_next << endl;
-    y_prev = y_curr;
-    y_curr = y_next;
+    vector<double> results;
+    results.push_back(start_value);
     
-
-    for (int i = 1; i < N - 1; i++) {
-        y_next = nonlinear(y_curr, y_prev, u[i], u[i-1]);
+    for (int i = 0; i < N - 1; i++) {
+        double y_next = nonlinear(results[i], (i > 0 ? results[i-1] : start_value), u[i], u[i]);
+        results.push_back(y_next);
         cout << "y" << i + 1 << " = " << y_next << endl;
-        y_prev = y_curr;
-        y_curr = y_next;
     }
 
-    cout << "\nНажмите Enter для выхода...";
+    cout << "\nРасчет завершен.";
     cin.get();
     return 0;
 }
