3ad1c64a251e1911d2da55f03d34c7bdde77881a
nklvmmr <z3594934z@gmail.com>
2025-11-26 21:11:36 +0300

    task_2 Николова

--- trunk/as06616/task2/src/func.cpp
+++ trunk/as06616/task2/src/func.cpp
@@ -1,6 +1,7 @@
 #include "func.h"
 #include <cmath>
 
+
 const double temperatureCoeff = 0.6;
 const double prevTemperatureCoeff = 0.3;
 const double warmCoeff = 0.8;
@@ -11,7 +12,10 @@ double linearmodel(double inputtemperature, double inputwarm) {
 }
 
 double nonlinearmodel(double inputtemperature, double prevtemperature, double inputwarm, double prevwarm) {
-    return temperatureCoeff * inputtemperature - prevTemperatureCoeff * pow(prevtemperature, 2) + warmCoeff * inputwarm + prevWarmCoeffSin * sin(prevwarm);
+    return temperatureCoeff * inputtemperature
+        - prevTemperatureCoeff * pow(prevtemperature, 2)
+        + warmCoeff * inputwarm
+        + prevWarmCoeffSin * sin(prevwarm);
 }
 
 double updateWarm(double baseWarm, int t) {
--- trunk/as06616/task2/src/main.cpp
+++ trunk/as06616/task2/src/main.cpp
@@ -2,38 +2,32 @@
 #include <cmath>
 #include "func.h"
 
-const double a = 0.6;
-const double b = 0.3;
-const double c = 0.8;
-const double d = 0.5;
+using namespace std;
 
 int main() {
     int time = 0;
     double inputtemperature = 0;
     double inputwarm = 0;
 
-    std::cout << "Введите количество шагов моделирования, начальную температуру y0 и начальный тепловой вход u0: ";
-    std::cin >> time 
-    std::cin >> inputtemperature;
-    std::cin >> inputwarm;
+    cout << "Введите количество шагов моделирования, начальное значение y0 и управляющее воздействие u0: ";
+    cin >> time >> inputtemperature >> inputwarm;
 
     double prevtemperature = inputtemperature;
     double prevwarm = inputwarm;
 
-    std::cout << "\nМоделирование линейной модели:\n";
+    cout << "\nЛинейная модель:\n";
     double y_linear = inputtemperature;
     for (int t = 1; t <= time; t++) {
-
         y_linear = linearmodel(y_linear, inputwarm);
-        std::cout << "Шаг " << t << ": y = " << y_linear << std::endl;
+        cout << "Шаг " << t << ": y = " << y_linear << endl;
     }
 
-    std::cout << "\nМоделирование нелинейной модели:\n";
+    cout << "\nНелинейная модель:\n";
     double y_nl = inputtemperature;
     for (int t = 1; t <= time; t++) {
         double currentWarm = updateWarm(inputwarm, t);
         double next = nonlinearmodel(y_nl, prevtemperature, currentWarm, prevwarm);
-        std::cout << "Шаг " << t << ": y = " << next << std::endl;
+        cout << "Шаг " << t << ": y = " << next << endl;
         prevtemperature = y_nl;
         y_nl = next;
         prevwarm = currentWarm;
