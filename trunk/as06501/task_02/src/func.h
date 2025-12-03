e2f34e21c6c8ac6466234856f28426836a524a0b
Вавдийчик Никита <vavdiychik01@gmail.com>
2025-12-04 01:10:31 +0300

    Update func.h

--- trunk/as06501/task_02/src/func.h
+++ trunk/as06501/task_02/src/func.h
@@ -3,12 +3,13 @@
 
 constexpr int N = 8;
 
-constexpr double A = 0.85;
-constexpr double B = 0.05;
-constexpr double C = 0.35;
-constexpr double D = 0.12;
+constexpr double a = 0.85;
+constexpr double b = 0.05;
+constexpr double c = 0.35;
+constexpr double d = 0.12;
 
-constexpr double Y_START = 10.0;
+constexpr double start_value = 10.0;
 
-double linearModel(double y, double u);
-double nonlinearModel(double yt, double yt_prev, double ut, double ut_prev);
+double linear(double y, double u);
+double nonlinear(double yt, double yt1, double ut, double ut1);
+
