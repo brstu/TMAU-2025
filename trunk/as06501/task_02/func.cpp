73360fa74731ea67e989a2d362fcef2ab640b815
Вавдийчик Никита <vavdiychik01@gmail.com>
2025-12-04 00:26:29 +0300

    Delete trunk/as06501/task_02/func.cpp

--- trunk/as06501/task_02/func.cpp
+++
@@ -1,9 +0,0 @@
-#include "func.h"
-
-double linearModel(double y, double u) {
-    return A * y + B * u;
-}
-
-double nonlinearModel(double yt, double yt_prev, double ut, double ut_prev) {
-    return A * yt - B * pow(yt_prev, 2) + C * ut + D * sin(ut_prev);
-}
