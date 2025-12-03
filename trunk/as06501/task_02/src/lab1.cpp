4e5394ff288c3a54c98c952b35a1c0eafe7f44cc
Вавдийчик Никита <vavdiychik01@gmail.com>
2025-12-04 00:20:02 +0300

    Delete trunk/as06501/task_02 directory

--- trunk/as06501/task_02/src/lab1.cpp
+++
@@ -1,9 +0,0 @@
-#include "lab1.h"
-#include <cmath>
-double linear(double y, double u) {
-    return a * y + b * u;
-}
-
-double nonlinear(double yt, double yt1, double ut, double ut1) {
-    return a * yt - b * yt1 * yt1 + c * ut + d * sin(ut1);
-}
