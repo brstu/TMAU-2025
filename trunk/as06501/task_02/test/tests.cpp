87430ed3fcd5fc1324634b671f415fc59943b407
Вавдийчик Никита <vavdiychik01@gmail.com>
2025-12-04 01:11:09 +0300

    Update tests.cpp

--- trunk/as06501/task_02/test/tests.cpp
+++ trunk/as06501/task_02/test/tests.cpp
@@ -2,49 +2,38 @@
 #include "func.h"
 
 TEST(LinearModel, ZeroInput) {
-    EXPECT_DOUBLE_EQ(linearModel(0, 0), 0);
+    EXPECT_DOUBLE_EQ(linear(0, 0), 0);
 }
 
 TEST(LinearModel, OnlyY) {
-    EXPECT_DOUBLE_EQ(linearModel(10, 0), A * 10);
+    EXPECT_DOUBLE_EQ(linear(10, 0), a * 10);
 }
 
 TEST(LinearModel, OnlyU) {
-    EXPECT_DOUBLE_EQ(linearModel(0, 5), B * 5);
+    EXPECT_DOUBLE_EQ(linear(0, 5), b * 5);
 }
 
 TEST(LinearModel, GeneralCase) {
-    EXPECT_DOUBLE_EQ(linearModel(10, 5), A * 10 + B * 5);
+    EXPECT_DOUBLE_EQ(linear(10, 4), a * 10 + b * 4);
 }
 
-// ------------------------
-// NONLINEAR MODEL TESTS
-// ------------------------
-
 TEST(NonlinearModel, ZeroInput) {
-    // В твоей формуле:
-    // A*0 - B*0^2 + C*0 + D*sin(0) = 0
-    EXPECT_DOUBLE_EQ(nonlinearModel(0, 0, 0, 0), 0);
+    EXPECT_DOUBLE_EQ(nonlinear(0, 0, 0, 0), 0);
 }
 
 TEST(NonlinearModel, OnlyY) {
-    // nonlinear(yt=10, yt1=10, ut=0, ut1=0)
-    // = A*10 - B*100
-    EXPECT_DOUBLE_EQ(nonlinearModel(10, 10, 0, 0),
-                     A * 10 - B * pow(10, 2));
+    EXPECT_DOUBLE_EQ(nonlinear(10, 10, 0, 0), a * 10 - b * pow(10, 2));
 }
 
 TEST(NonlinearModel, OnlyU) {
-    // nonlinear(yt=0, yt1=0, ut=5, ut1=5)
-    // = C*5 + D*sin(5)
-    EXPECT_DOUBLE_EQ(nonlinearModel(0, 0, 5, 5),
-                     C * 5 + D * sin(5));
+    EXPECT_DOUBLE_EQ(nonlinear(0, 0, 4, 4), c * 4 + d * sin(4));
 }
 
 TEST(NonlinearModel, GeneralCase) {
-    // nonlinear(yt=10, yt1=10, ut=5, ut1=5)
-    EXPECT_DOUBLE_EQ(nonlinearModel(10, 10, 5, 5),
-                     A * 10 - B * pow(10, 2) + C * 5 + D * sin(5));
+    EXPECT_DOUBLE_EQ(
+        nonlinear(10, 10, 4, 4),
+        a * 10 - b * pow(10, 2) + c * 4 + d * sin(4)
+    );
 }
 
 int main(int argc, char** argv) {
