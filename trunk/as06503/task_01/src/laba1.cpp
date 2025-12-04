26f5d0ba2ec8ecba902e26f2e0116859d78d440e
Елизавета Неруш <elizaveta.nerush@gmail.com>
2025-12-04 12:36:00 +0300

    Задание 1 испр 2

--- trunk/as06503/task_01/src/laba1.cpp
+++ trunk/as06503/task_01/src/laba1.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <array>
+
 using std::cout;
 using std::endl;
+
 // constants
 const int n = 10; // time steps
 const double a = 0.99;
@@ -23,17 +26,22 @@ double nonlinear(double yt, double yt_1, double ut, double ut_1) {
 int main() { 
     cout << "Linear model" << endl;
     cout << "y0 = " << Y0 << endl;
-    double u[n] = { 5,7,6,5,7,6,5,7,6,5 };
+
+    std::array<double, n> u = { 5,7,6,5,7,6,5,7,6,5 };
+
     double yt = Y0;
     for (int i = 0; i < n; i++) {
         yt = linear(yt, u[i]);
         cout << "y" << i + 1 << " = " << yt << endl;
     }
+
     cout << "\n";
     double yt_1 = Y0;
     yt = Y0;
+
     cout << "Nonlinear model" << endl;
     cout << "y0 = " << Y0 << endl;
+
     for (int i = 0; i < n; i++) {
         double ut = u[i];
         double ut_1 = (i == 0) ? u[0] : u[i - 1];
@@ -42,5 +50,6 @@ int main() {
         yt_1 = yt;
         yt = yt_new;
     }
+
     return 0;
 }
