7b52bac9f6b63bf26a8bc7d10eb4c51e301a4492
nikitka510 <egorenkovnikita0@gmail.com>
2025-12-05 10:10:28 +0300

    [task_01] final first

--- trunk/as006505/task_01/src/main.cpp
+++ trunk/as006505/task_01/src/main.cpp
@@ -1,78 +1,62 @@
 #include <iostream>
 #include <cmath>
-#include <iomanip>
-#include <vector>
 
-using namespace std;
-
-float linear_model(float y_prev, float u_tau, float a, float b) {
-    float y_next = a * y_prev + b * u_tau;
-    return y_next;
+double nextLinear(double a, double b, double u, double y) {
+    return a * y + b * u;
 }
 
-float nonlinear_model(float y_prev, float y_prev_2, float u_prev, 
-                     float u_tau, float a, float b, float c, float d) {
-    float y_next = a * y_prev - b * pow(y_prev_2, 2) + c * u_tau + d * sin(u_prev);
-    return y_next;
+void simulateLinear(double a, double b, double u, int steps) {
+    std::cout << "Линейная модель" << std::endl;
+    double y = 0.0;
+    for (int i = 0; i < steps; i++) {
+        std::cout << "τ=" << i << ": y=" << y << std::endl;
+        y = nextLinear(a, b, u, y);
+    }
+}
+
+struct NonlinearParams {
+    double a;
+    double b;
+    double c;
+    double d;
+};
+
+double nextNonlinear(const NonlinearParams& params, double u_prev, double y, double y_prev) {
+    return params.a * y - params.b * (y_prev * y_prev) + params.c * u_prev + params.d * std::sin(u_prev);
+}
+
+void simulateNonlinear(double a, double b, double c, double d, double u, int steps) {
+    std::cout << "Нелинейная модель" << std::endl;
+    double y = 0.0;
+    double y_prev = 0.0;
+    double u_prev = 0.0;
+    
+    NonlinearParams params{a, b, c, d};
+    
+    for (int i = 0; i < steps; i++) {
+        std::cout << "τ=" << i << ": y=" << y << std::endl;
+        double y_next = nextNonlinear(params, u_prev, y, y_prev);
+        y_prev = y;
+        y = y_next;
+        u_prev = u; 
+    }
 }
 
 int main() {
-    const float a = 0.8, b = 0.2, c = 0.25, d = 0.1;
-    float u_prev = 0, u_tau;
-    int n;
-    float y_prev = 20, y_prev_2 = 20;
-    
-    vector<float> u_vec;
-    vector<float> linear_result;
-    vector<float> nonlinear_result;
+    double a1 = 0.3;
+    double b1 = 0.3;
+    double u1 = 0.9;
+    int n1 = 10;
+    simulateLinear(a1, b1, u1, n1);
+    std::cout << std::endl;
 
-    cout << "=== Temperature Control Object Simulation ===" << endl;
-    cout << "Linear model: y_{τ+1} = a*y_τ + b*u_τ" << endl;
-    cout << "Nonlinear model: y_{τ+1} = a*y_τ - b*y_{τ-1}² + c*u_τ + d*sin(u_{τ-1})" << endl;
-    cout << "Parameters: a=" << a << ", b=" << b << ", c=" << c << ", d=" << d << endl;
-    cout << "Initial temperature: " << y_prev << "°C" << endl << endl;
-
-    cout << "Enter number of steps: ";
-    cin >> n;
-    cout << "Enter control inputs u(τ) separated by spaces:" << endl;
-    for (int i = 0; i < n; i++) {
-        cin >> u_tau;
-        u_vec.push_back(u_tau);
-    }
-
-    float y_prev_linear = y_prev;
-    float y_prev_nonlinear = y_prev;
-
-    // Beautiful table header
-    cout << endl << "Simulation Results:" << endl;
-    cout << string(60, '-') << endl;
-    cout << setw(5) << "Step" << setw(10) << "u(τ)" 
-         << setw(20) << "Linear Model" << setw(20) << "Nonlinear Model" << endl;
-    cout << string(60, '-') << endl;
-
-    for (int i = 0; i < n; i++) {
-        u_tau = u_vec[i];
-
-        float linear_temp = linear_model(y_prev_linear, u_tau, a, b);
-        float nonlinear_temp = nonlinear_model(y_prev_nonlinear, y_prev_2,
-                                            u_prev, u_tau, a, b, c, d);
-        
-        linear_result.push_back(linear_temp);
-        nonlinear_result.push_back(nonlinear_temp);
-
-        // Beautiful table output
-        cout << setw(5) << i + 1 
-             << setw(10) << fixed << setprecision(2) << u_tau
-             << setw(20) << fixed << setprecision(3) << linear_temp
-             << setw(20) << fixed << setprecision(3) << nonlinear_temp 
-             << endl;
-
-        y_prev_linear = linear_temp;
-        y_prev_2 = y_prev_nonlinear;
-        y_prev_nonlinear = nonlinear_temp;
-        u_prev = u_tau;
-    }
-    cout << string(60, '-') << endl;
+    double a2 = 0.1;
+    double b2 = 0.2;
+    double c2 = 0.4;
+    double d2 = 0.2;
+    double u2 = 0.8;
+    int n2 = 10;
+    simulateNonlinear(a2, b2, c2, d2, u2, n2);
 
     return 0;
 }
