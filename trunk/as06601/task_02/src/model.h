fcd1718b3c2155ace70226cadede24e18d7601af
Максим <maksim@192.168.100.4>
2025-12-09 00:07:49 +0300

    her

--- trunk/as06601/task_02/src/model.h
+++ trunk/as06601/task_02/src/model.h
@@ -1,11 +1,14 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-// Линейная модель: y[t+1] = a*y[t] + b*u[t]
-double linearModel(double a, double b, double y_t, double u_t);
+struct NonlinearParams {
+    double a;
+    double b_nl;
+    double c;
+    double d;
+};
 
-// Нелинейная модель: y[t+1] = a*y[t] - b_nl*y[t]^2 + c*u[t] + d*sin(u_prev)
-double nonlinearModel(double a, double b_nl, double c, double d,
-                      double y_t, double u_t, double u_prev);
+double linearModel(double a, double b, double y_t, double u_t);
+double nonlinearModel(const NonlinearParams& params, double y_t, double u_t, double u_prev);
 
 #endif // MODEL_H
