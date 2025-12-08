5485d805eade885df9d01e95f2e0e3863f0f41f5
Кисель Максим <maximk220066@icloud.com>
2025-12-08 16:35:01 +0300

    Update func.cpp

--- trunk/as06508/task_02/src/func.cpp
+++ trunk/as06508/task_02/src/func.cpp
@@ -1,17 +1,17 @@
 ﻿#include "func.h"
 #include <cmath>
 
-// Один шаг линейной модели
+// One step of the linear model
 double linearStep(double y, double u, const ModelParams& params) {
     return params.a * y + params.b * u;
 }
 
-// Один шаг нелинейной модели
+// One step of the nonlinear model
 double nonlinearStep(double y, double y_prev, double u, double u_prev, const ModelParams& params) {
     return params.a * y - params.b * std::pow(y_prev, 2) + params.c * u + params.d * std::sin(u_prev);
 }
 
-// Симуляция линейной модели
+// Simulation of a linear model
 std::vector<double> simulateLinear(const ModelParams& params, const std::vector<double>& u) {
     std::vector<double> y(params.time_steps + 1);
     y[0] = params.initial_temp;
@@ -21,7 +21,7 @@ std::vector<double> simulateLinear(const ModelParams& params, const std::vector<
     return y;
 }
 
-// Симуляция нелинейной модели
+// Simulation of a nonlinear model
 std::vector<double> simulateNonlinear(const ModelParams& params, const std::vector<double>& u) {
     std::vector<double> y(params.time_steps + 1);
     y[0] = params.initial_temp;
@@ -32,3 +32,4 @@ std::vector<double> simulateNonlinear(const ModelParams& params, const std::vect
     }
     return y;
 }
+
