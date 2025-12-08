d77eae39abef0d16b3730f866bbda00300386758
Максим <maksim@192.168.100.4>
2025-12-09 00:07:49 +0300

    her

--- trunk/as06601/task_02/src/model.cpp
+++ trunk/as06601/task_02/src/model.cpp
@@ -5,7 +5,6 @@ double linearModel(double a, double b, double y_t, double u_t) {
     return a * y_t + b * u_t;
 }
 
-double nonlinearModel(double a, double b_nl, double c, double d,
-                      double y_t, double u_t, double u_prev) {
-    return a * y_t - b_nl * y_t * y_t + c * u_t + d * sin(u_prev);
+double nonlinearModel(const NonlinearParams& p, double y_t, double u_t, double u_prev) {
+    return p.a * y_t - p.b_nl * y_t * y_t + p.c * u_t + p.d * sin(u_prev);
 }
