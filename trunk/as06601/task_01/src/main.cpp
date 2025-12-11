6da3fc3b2158b4bf4a1ecfc632e73e13dfcbb6ab
Максим <maksim@MacBook-Pro-Maksim.local>
2025-12-11 15:22:03 +0300

    SQ fix1

--- trunk/as06601/task_01/src/main.cpp
+++ trunk/as06601/task_01/src/main.cpp
@@ -4,15 +4,33 @@
 
 using namespace std;
 
+// Параметры нелинейной модели
+struct NonlinearParams {
+    double a;
+    double b;
+    double c;
+    double d;
+};
+
+// Состояние (выходы и входы)
+struct State {
+    double y_t;
+    double y_prev;
+    double u_t;
+    double u_prev;
+};
+
 // Линейная модель: y[t+1] = a*y[t] + b*u[t]
 double linearModel(double a, double b, double y_t, double u_t) {
     return a * y_t + b * u_t;
 }
 
-// Нелинейная модель: y[t+1] = a*y[t] - b*y[t-1]^2 + c*u[t] + d*sin(u[t-1])
-double nonlinearModel(double a, double b, double c, double d,
-                      double y_t, double y_prev, double u_t, double u_prev) {
-    return a * y_t - b * y_prev * y_prev + c * u_t + d * sin(u_prev);
+// Нелинейная модель
+double nonlinearModel(const NonlinearParams& p, const State& s) {
+    return p.a * s.y_t
+         - p.b * s.y_prev * s.y_prev
+         + p.c * s.u_t
+         + p.d * sin(s.u_prev);
 }
 
 int main() {
@@ -20,24 +38,28 @@ int main() {
     cout << "Введите количество шагов моделирования: ";
     cin >> n;
 
-    // Константы (можно менять)
-    double a = 0.8, b = 0.1, c = 0.05, d = 0.02;
+    // Константы (разделены по требованиям Sonar)
+    double a = 0.8;
+    double b = 0.1;
+    double c = 0.05;
+    double d = 0.02;
+
+    NonlinearParams params{a, b, c, d};
 
     // Начальные условия
-    double y0 = 20.0;    // начальная температура объекта
-    double u0 = 5.0;     // начальное управляющее воздействие
+    double y0 = 20.0; // начальная температура объекта
+    double u0 = 5.0;  // начальное управляющее воздействие
 
     vector<double> y(n+1);
     vector<double> u(n+1);
 
-    // Заполняем вход (например, постоянное воздействие u = u0)
+    // Вход
     for (int t = 0; t <= n; t++) {
         u[t] = u0;
     }
 
-    // Начальные значения температуры
     y[0] = y0;
-    if (n > 0) y[1] = y0; // чтобы была "память" для нелинейной модели
+    if (n > 0) y[1] = y0;
 
     cout << "Результаты моделирования:\n";
 
@@ -51,9 +73,11 @@ int main() {
     // Нелинейная модель
     cout << "\n--- Нелинейная модель ---\n";
     y[0] = y0;
-    if (n > 0) y[1] = y0; // снова задаем старт
+    if (n > 0) y[1] = y0;
+
     for (int t = 1; t < n; t++) {
-        y[t+1] = nonlinearModel(a, b, c, d, y[t], y[t-1], u[t], u[t-1]);
+        State st{y[t], y[t-1], u[t], u[t-1]};
+        y[t+1] = nonlinearModel(params, st);
         cout << "t=" << t+1 << "  y=" << y[t+1] << endl;
     }
 
