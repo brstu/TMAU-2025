b3df58822bceb4269bd71e02a6bc5b5800561d41
Мария Сергиевич <msergievich3@gmail.com>
2025-12-11 10:31:13 +0300

    Update laba1_tmau.cpp

--- trunk/as06505/task_01/src/laba1_tmau.cpp
+++ trunk/as06505/task_01/src/laba1_tmau.cpp
@@ -1,13 +1,17 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
-using namespace std;
+#include <vector>  
+#include <array>  
 
-int n = 10;
+using namespace std;
+const int n = 10;
 const double a = 0.9;
 const double b = 0.001;
 const double c = 0.4;
 const double d = 0.1;
-double ini_val = 10;
+const double ini_val = 10.0;
+
+const std::array<double, n> u = { 10.0, 8.0, 12.0, 9.0, 11.0, 10.0, 8.0, 12.0, 9.0, 11.0 };
 
 double linear(double yt, double ut) {
     return a * yt + b * ut;
@@ -19,28 +23,37 @@ double nonlinear(double yt, double yt_1, double ut, double ut_1) {
 
 int main() {
     setlocale(LC_ALL, "RU");
+    
     cout << "Линейная модель" << endl;
     cout << "y0 = " << ini_val << endl;
-    double u[10] = { 10,8,12,9,11,10,8,12,9,11 };
-    double yt = ini_val, yt_1 = ini_val;
+    
+    double yt = ini_val;
+    
+    double yt_prev = ini_val;
 
+    // Линейная модель
     for (int i = 0; i < n; i++) {
         yt = linear(yt, u[i]);
         cout << "y" << i + 1 << " = " << yt << endl;
     }
 
     cout << "\n";
-    yt_1 = yt = ini_val;
+    
+    yt = ini_val;
+    yt_prev = ini_val;
+    
     cout << "Нелинейная модель" << endl;
     cout << "y0 = " << ini_val << endl;
-
+    
     for (int i = 0; i < n - 1; i++) {
         double temp = yt;
-        yt = nonlinear(yt, yt_1, u[i + 1], u[i]);
-        yt_1 = temp;
+        yt = nonlinear(yt, yt_prev, u[i + 1], u[i]);
+        yt_prev = temp;
         cout << "y" << i + 1 << " = " << yt << endl;
     }
 
-    system("pause");
+    cout << "\nНажмите Enter для выхода...";
+    cin.get();
+    
     return 0;
 }
