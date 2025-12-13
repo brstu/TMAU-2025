55d5b26a943c1ca6085784dd2337cb08a3cc17f9
Мария Сергиевич <msergievich3@gmail.com>
2025-12-13 23:57:47 +0300

    Update func.cpp

--- trunk/as06505/task_02/src/func.cpp
+++ trunk/as06505/task_02/src/func.cpp
@@ -5,5 +5,5 @@ double linear(double yt, double ut) {
 }
 
 double nonlinear(double yt, double yt_1, double ut, double ut_1) {
-    return a * yt - b * pow(yt_1, 2) + c * ut + d * sin(ut_1);
+    return a * yt - b * (yt_1 * yt_1) + c * ut + d * sin(ut_1);
 }
