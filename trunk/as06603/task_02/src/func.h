74235c2d4aa1e51335d59aed6f9f32d7718532d1
Vika16-7 <vikagol2005@gmail.com>
2025-12-19 09:25:50 +0300

    Головкина 2 исправлен

--- trunk/as06603/task_02/src/func.h
+++ trunk/as06603/task_02/src/func.h
@@ -1,14 +1,19 @@
 #pragma once
 #include <cmath>
 
-// Параметры модели (объявления)
-extern const int STEPS;
-extern const double a;
-extern const double b;
-extern const double c;
-extern const double d;
-extern const double Y0;
+// Константы модели управления
+// Эти значения задаются в func.cpp
+extern const int    STEPS;   // количество шагов моделирования
+extern const double a;       // коэффициент при y
+extern const double b;       // коэффициент при u или y^2
+extern const double c;       // коэффициент при управляющем воздействии
+extern const double d;       // коэффициент при синусе
+extern const double Y0;      // начальное значение температуры
 
-// Прототипы функций модели
-double linear(double y, double u);
-double nonlinear(double y, double y_prev, double u, double u_prev);
+namespace Model {
+    // Линейная модель: y_{t+1} = a*y_t + b*u_t
+    double linearModel(double y, double u);
+
+    // Нелинейная модель: y_{t+1} = a*y_t - b*y_{t-1}^2 + c*u_t + d*sin(u_{t-1})
+    double nonlinearModel(double y, double y_prev, double u, double u_prev);
+}
