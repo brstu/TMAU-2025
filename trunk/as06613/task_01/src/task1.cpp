0c91599e5517dc2885610ab48107078946285401
peacewxrld <alchimeist@gmail.com>
2025-12-23 22:51:05 +0300

    lab1

---
+++ trunk/as06613/task_01/src/task1.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip> 
+
+class TemperatureModel {
+private:
+    double a;
+    double b;
+    double c;
+    double d;
+    double y_prev = 0;
+    double y_prev2 = 0;
+    double u_prev = 0;
+    bool is_nonlinear;
+
+public:
+  
+    TemperatureModel(double a_val, double b_val)
+        : a(a_val), b(b_val), c(0), d(0), is_nonlinear(false) {
+    }
+
+   
+    TemperatureModel(double a_val, double b_val, double c_val, double d_val)
+        : a(a_val), b(b_val), c(c_val), d(d_val), is_nonlinear(true) {
+    }
+
+    void setInitialConditions(double y0, double y1 = 0, double u0 = 0) {
+        y_prev = y0;
+        y_prev2 = y1;
+        u_prev = u0;
+    }
+
+    double calculateNext(double u_current) {
+        double y_next;
+
+        if (is_nonlinear) {
+      
+            y_next = a * y_prev - b * y_prev2 * y_prev2 + c * u_current + d * sin(u_prev);
+        }
+        else {
+           
+            y_next = a * y_prev + b * u_current;
+        }
+
+   
+        y_prev2 = y_prev;
+        y_prev = y_next;
+        u_prev = u_current;
+
+        return y_next;
+    }
+
+    double getCurrentTemperature() const {
+        return y_prev;
+    }
+};
+
+void testLinearModel() {
+    std::cout << "=== LINEAR MODEL TEST ===" << std::endl;
+
+    double a = 0.8;
+    double b = 0.2;
+
+    TemperatureModel model(a, b);
+    model.setInitialConditions(20.0);
+
+    std::vector<double> control_inputs = { 10, 15, 20, 25, 30, 25, 20, 15, 10, 5 };
+
+    std::cout << std::setw(4) << "Time" << std::setw(8) << "Control" << std::setw(12) << "Temperature" << std::endl;
+    std::cout << "0\t-\t20.0" << std::endl;
+
+    for (size_t t = 0; t < control_inputs.size(); ++t) {
+        double u = control_inputs[t];
+        double y = model.calculateNext(u);
+        std::cout << std::fixed << std::setprecision(1) << std::setw(4) << t + 1 << std::setw(8) << u << std::setw(12) << y << std::endl;
+    }
+}
+
+void testNonlinearModel() {
+    std::cout << "\n=== NONLINEAR MODEL TEST ===" << std::endl;
+
+    double a = 0.7;
+    double b = 0.01;
+    double c = 0.3;
+    double d = 0.1;
+
+    TemperatureModel model(a, b, c, d);
+    model.setInitialConditions(20.0, 19.5, 5.0);
+
+    std::vector<double> control_inputs = { 10, 15, 20, 25, 30, 25, 20, 15, 10, 5 };
+
+    std::cout << std::setw(4) << "Time" << std::setw(8) << "Control" << std::setw(12) << "Temperature" << std::endl;
+    std::cout << "0\t5.0\t20.0" << std::endl;
+
+    for (size_t t = 0; t < control_inputs.size(); ++t) {
+        double u = control_inputs[t];
+        double y = model.calculateNext(u);
+        std::cout << std::fixed << std::setprecision(3) << std::setw(4) << t + 1 << std::setw(8) << u << std::setw(12) << y << std::endl;
+    }
+}
+
+int main() {
+    testLinearModel();
+    testNonlinearModel();
+    return 0;
+}
