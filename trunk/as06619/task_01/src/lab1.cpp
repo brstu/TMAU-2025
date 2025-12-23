a3a388a94f9d35e54d22d7413f5b0810f7b4c309
Nastya <204497599+Anastasia05-j@users.noreply.github.com>
2025-12-23 23:50:23 +0300

    number1

--- trunk/as06619/task_01/src/lab1.cpp
+++ trunk/as06619/task_01/src/lab1.cpp
@@ -4,31 +4,26 @@
 
 using namespace std;
 
-struct NonlinearParams {
-    double y_t;
-    double y_prev;
-    double u_t;
-    double u_prev;
+struct ModelCoeffs {
     double a;
     double b;
     double c;
     double d;
 };
 
-double compute_linear_model(double y_t, double u_t, double a, double b) {
-    return a * y_t + b * u_t;
+double compute_linear_model(double y_t, double u_t, const ModelCoeffs& coeffs) {
+    return coeffs.a * y_t + coeffs.b * u_t;
 }
 
-double compute_nonlinear_model(const NonlinearParams& p) {
-    return p.a * p.y_t - p.b * p.y_prev * p.y_prev + p.c * p.u_t + p.d * sin(p.u_prev);
+double compute_nonlinear_model(double y_t, double y_prev, 
+                               double u_t, double u_prev,
+                               const ModelCoeffs& coeffs) {
+    return coeffs.a * y_t - coeffs.b * y_prev * y_prev 
+           + coeffs.c * u_t + coeffs.d * sin(u_prev);
 }
 
 int main() {
-    double a = 0.8;
-    double b = 0.2;
-    double c = 0.1;
-    double d = 0.05;
-
+    ModelCoeffs coeffs = {0.8, 0.2, 0.1, 0.05};
     int N = 20;
 
     vector<double> y(N + 1, 0.0);
@@ -40,7 +35,7 @@ int main() {
 
     cout << "Linear model:" << endl;
     for (int t = 0; t < N; t++) {
-        y[t + 1] = compute_linear_model(y[t], u[t], a, b);
+        y[t + 1] = compute_linear_model(y[t], u[t], coeffs);
         cout << "t = " << t + 1 << "   y = " << y[t + 1] << endl;
     }
 
@@ -48,17 +43,7 @@ int main() {
 
     cout << "\nNonlinear model:" << endl;
     for (int t = 1; t < N; t++) {
-        NonlinearParams p;
-        p.y_t = y[t];
-        p.y_prev = y[t - 1];
-        p.u_t = u[t];
-        p.u_prev = u[t - 1];
-        p.a = a;
-        p.b = b;
-        p.c = c;
-        p.d = d;
-
-        y[t + 1] = compute_nonlinear_model(p);
+        y[t + 1] = compute_nonlinear_model(y[t], y[t - 1], u[t], u[t - 1], coeffs);
         cout << "t = " << t + 1 << "   y = " << y[t + 1] << endl;
     }
 
