c27703bed9b3b19885ebd4567df96eda9d5a1f68
arseniiOsovets <arsenasavets@gmail.com>
2025-12-24 09:32:11 +0300

    wip

--- trunk/as06617/task_02/src/func.cpp
+++ trunk/as06617/task_02/src/func.cpp
@@ -1,25 +1,16 @@
 #include "func.h"
 #include <cmath>
 
-namespace model_params {
-    constexpr int    STEPS_COUNT = 10;
-    constexpr double A = 0.99;
-    constexpr double B = 0.01;
-    constexpr double C = 0.5;
-    constexpr double D = 0.1;
-    constexpr double INITIAL_Y = 18.0;
-}
-
 // Линейная модель
-inline double linear(double y, double u) {
+double linear(double y, double u) {
     return model_params::A * y + model_params::B * u;
 }
 
 // Нелинейная модель
-inline double nonlinear(double y,
-                         double y_prev,
-                         double u,
-                         double u_prev) {
+double nonlinear(double y,
+                 double y_prev,
+                 double u,
+                 double u_prev) {
     return model_params::A * y
          - model_params::B * (y_prev * y_prev)
          + model_params::C * u
